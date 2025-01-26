66e72366af413fba57483cb9b5301e2a1aad7089
surajgsn07 <120456653+surajgsn07@users.noreply.github.com>
2025-01-26 18:04:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin:"https://hackmeets.vercel.app",
+    origin:"https://hack-meets.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
